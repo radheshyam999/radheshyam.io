fa7c5115b4aedc8458fddb21fcadd82e39cee378
radheshyam999 <radheshyamdubey999@gmail.com>
2019-06-26 16:47:01 +0530

    need to  add nav  some other pages

--- js/main.js
+++ js/main.js
@@ -3,5 +3,18 @@
        //document.getElementById("program").innerHTML =valueofhi;
    }
 
- 
+
     //valuepro.innerHTML= "hiiiiiiiiii";
+    /** on call it is going to acess from data**/
+
+    function Acesslinkdean(){
+
+       var url =" https://www.linkedin.com/in/radheshyam-dubey-2b37a6133/";
+       }
+      function AcesshackerRank() {
+          var url = "https://www.hackerrank.com/radheshyamdubey1";
+      }
+      function Acessgithub() {
+
+          var url = "https://github.com/radheshyam999";
+      }
